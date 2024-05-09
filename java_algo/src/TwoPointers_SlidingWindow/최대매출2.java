16af354934f6e567f1a15d188f19491741bce4d1
Daae-Kim <wlsemrdl1212@naver.com>
2024-05-09 23:31:35 +0900

    최대매출문제_window를 이용해서 풀기

---
+++ java_algo/src/TwoPointers_SlidingWindow/최대매출2.java
@@ -0,0 +1,29 @@
+package TwoPointers_SlidingWindow;
+
+import java.util.Scanner;
+
+public class 최대매출2 {
+    public int solution(int n, int k, int[] arr){
+        int answer=0;
+        int sum =0;
+        for(int i=0; i<k; i++){
+            sum += arr[i];
+        }
+        for(int i=k; i<n; i++){
+            sum+=(arr[i]-arr[i-k]);
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+    public static void main(String args[]){
+        최대매출2 T = new 최대매출2();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+    }
+}
