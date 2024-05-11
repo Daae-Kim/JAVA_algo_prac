38dfff1fdb13f88c8c5fcbec97eaf1c688ff3df6
Daae-Kim <wlsemrdl1212@naver.com>
2024-05-11 22:52:05 +0900

    연속부분수열

--- java_algo/src/TwoPointers_SlidingWindow/연속부분수열.java
+++ java_algo/src/TwoPointers_SlidingWindow/연속부분수열.java
@@ -1,2 +1,33 @@
-package TwoPointers_SlidingWindow;public class 연속부분수열 {
+package TwoPointers_SlidingWindow;
+
+import java.util.Scanner;
+
+public class 연속부분수열 {
+    public int solution(int n, int m, int arr[]){
+        //12131112
+        // sum < 6 -> rt , sum >= 6 -> lt
+        int lt =0;
+        int answer =0; int sum = 0;
+        for(int rt=0; rt<n; rt++){
+            sum += arr[rt];
+            if(sum == m ) answer++;
+            while(sum >= m){
+                sum -= arr[lt];
+                lt++;
+                if(sum == m) answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        연속부분수열 T = new 연속부분수열();
+        System.out.print(T.solution(n, m, arr));
+    }
 }
